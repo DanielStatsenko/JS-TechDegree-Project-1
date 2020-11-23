ec7f7cdc836e9aafa33f3e2d0f6e41d439cc10dd
DanielStatsenko <74886473+DanielStatsenko@users.noreply.github.com>
2020-11-23 15:29:44 -0500

    Updated Quotes Array In js\scripts.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,38 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+let quotes = [
+  {
+    quote: 'We are what we pretend to be, so we must be careful about what we pretend to be.',
+    source: 'Kurt Vonnegut',
+    citation: 'Mother Night',
+    year: 1999
+},
+  {    
+    quote: 'The flower that blooms in adversity is the rarest and most beautiful of all',
+    source: 'Walt Disney Company',
+    citation: 'Mulan',
+    year: 1998
+  },
+  {
+    quote: "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
+    source: 'Roy T. Bennett',
+    citation: 'The Light in the Heart',
+    year: 2016
+  },
+  {
+    quote: 'Instead of worrying about what you cannot control, shift your energy to what you can create.',
+    source: 'Roy T. Bennett',
+    citation: 'The Light in the Heart',
+    year: 2016
+  },
+  {
+    quote: 'Pain is temporary. Quitting lasts forever.',
+    source: 'Lance Armstrong Sally Jenkins',
+    citation: 'Every Second Counts',
+    year: 2003
+  }
+]
 
 
 /***
